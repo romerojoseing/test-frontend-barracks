f8b8c79b5fc842d041cf5fae5f0d542d35117bec
Romero José <romerojoseing@gmail.com>
2021-09-03 14:05:29 -0400

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 /* ---------- Connection with API ---------- */
 export const environment = {
   production: true,
-  urlTest: 'https://testing.menooservices.com/api/Store/ItemsForExercise'
+  loginBarracks: 'https://beta-api.barracks.gg/v2/Auth/Authenticate',
+  nasaAPI: 'https://api.nasa.gov/planetary/apod?api_key=zTtpDopXhclt3WZTqdqhbhdTgL9u6UfxR1xuOLFk&count=3',
+  pokemonAPI: 'https://pokeapi.co/api/v2/pokemon/'
 };
